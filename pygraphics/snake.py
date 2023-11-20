385351f64b75268800bbe05169eaea39c6b7c6d2
saidino84 <tylorguy2018@gmail.com>
2023-11-20 18:06:09 -0200

    Aplicado OOP for snake game

--- pygraphics/snake.py
+++ pygraphics/snake.py
@@ -3,56 +3,62 @@ from pygame.locals import *
 from random import randint 
 
 pygame.init()
-height=400
-width=600
-LEFT=4
-RIGHT=6
-UP=8
-DOWN=2
-SPEED_UP=5
 
-screen=pygame.display.set_mode((width,height))
-pygame.display.set_caption("snake")
+from enum import Enum
+class Direction(Enum):
+    UP=8
+    DOWN=2
+    LEFT=4
+    RIGHT=6
 
-snake_pos=[(200,200),(210,200),(220,200)]
-snake_skin=pygame.Surface((10,10))
-snake_skin.fill((255,255,255))
+class Food:
+    def __init__(self,width:int,height:int ,color=(255,0,0)):
+        self.color=color
+        self.size=(20,20)
+        self.pos=(randint(20,width), randint(50,height))
+        self.graphic=pygame.Surface(self.size)
+        self.graphic.fill(self.color)
+class Snake:
+    def __init__(self,color=(255,255,255)):
+        self.color=color
+        self.pos=[(200,150),(210,150),(220,150)]
+        self.size=(10,10)
+        self.graphic=pygame.Surface(self.size)
+class Game:
+    def __init__(self,food:Food,snake:Snake,time=20, width:int=600,height:int=400):
+        self.height=height
+        self.width=height
+        self.screen=pygame.display.set_mode((self.width,self.height))
+        self.snake=snake
+        self.food=food
+        self.is_running=True
+        self.time=time
+        self.fps=pygame.time.Clock()
+        self.direction=Direction.UP
 
-food_pos=(randint(0,width),randint(0,590))
-food=pygame.Surface((10,10))
-food.fill((255,15,15))
-
-direction=LEFT
-
-
-fps=pygame.time.Clock()
-def start_rolling():
-    while True:
-        fps.tick(20)
-        for event in pygame.event.get():
-            if event.type ==pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key==pygame.K_RIGHT or pygame.K_6:
-                    print('RIGHT')
-                    direction=RIGHT
-                if event.key==pygame.K_LEFT or pygame.K_4:
-                    print('LEFT')
-                    direction=LEFT
-                if event.key==pygame.K_UP or pygame.K_8:
-                    print('UP')
-                    direction=UP
-                if event.key==pygame.K_DOWN or pygame.K_2:
-                    print('DOWN')
-                    direction=DOWN
-        for pos in range(len(snake_pos)-1,0,-1):
-            snake_pos[pos]=(snake_pos[pos-1][0],snake_pos[pos-1][1])
-        screen.fill((0,0,0))
-        screen.blit(food,food_pos)
-        for pos in snake_pos:
-            screen.blit(snake_skin,pos)
-        pygame.display.update()
-
-# if __name__=="__main__":
-start_rolling()
-
+        self.start()
+    def start(self):
+        while self.is_running:
+            self.fps.tick(self.time)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    quit()
+            
+                if event.type ==KEYDOWN:
+                    print("Going Down ...")
+            self.screen.blit(self.food.graphic,(25,25))
+                        
+            self.screen.fill((36,33,33))
+            # self.screen.fill((0,0,0))
+            self.screen.blit(self.food.graphic,self.food.pos)
+            # pygame.display.flip()
+            self.food.pos=(self.food.pos[0],self.food.pos[1]+1)
+            pygame.display.update()
+        
+def main():
+    width=600
+    height=400
+    food=Food(width=width,height=height)
+    snake=Snake()
+    game = Game(food=food,snake=snake)
+main()
