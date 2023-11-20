edd5e77ee649fd8ab9613ef07131bb28f824b6df
saidino84 <tylorguy2018@gmail.com>
2023-11-20 19:17:12 -0200

    key event start implemenying... on snake

--- pygraphics/snake.py
+++ pygraphics/snake.py
@@ -18,12 +18,34 @@ class Food:
         self.pos=(randint(20,width), randint(50,height))
         self.graphic=pygame.Surface(self.size)
         self.graphic.fill(self.color)
+        self.direction=Direction.LEFT
+    
 class Snake:
     def __init__(self,color=(255,255,255)):
         self.color=color
         self.pos=[(200,150),(210,150),(220,150)]
-        self.size=(10,10)
+        self.size=(20,20)
         self.graphic=pygame.Surface(self.size)
+        self.graphic.fill(self.color)
+    def change_direction(self,direction:Direction=Direction.RIGHT):
+        match direction:
+            case Direction.LEFT:
+                for pos in self.pos:
+                    self.pos[pos]=(self.pos[pos][0]+1,self.pos[pos][1])
+            case Direction.RIGHT:
+                for pos in self.pos:
+                    self.pos[pos]=(self.pos[pos][0]-1 ,self.pos[pos][1])
+            case Direction.UP:
+                for pos in self.pos:
+                    self.pos[pos]=(self.pos[pos][0] ,self.pos[pos][1]-1)
+            case Direction.DOWN:
+                for pos in  range(len(self.pos)):
+                    # self.pos[pos]=(self.pos[pos][0] ,self.pos[pos][1]+1)
+                    print(self.pos[pos][0])
+                    self.pos[pos]=(self.pos[pos][0],self.pos[pos][1]+2)
+                 
+            case _:
+                print('Not wolking')
 class Game:
     def __init__(self,food:Food,snake:Snake,time=20, width:int=600,height:int=400):
         self.height=height
@@ -45,14 +67,22 @@ class Game:
                     quit()
             
                 if event.type ==KEYDOWN:
-                    print("Going Down ...")
+                    self.snake.change_direction(direction=Direction.DOWN)
             self.screen.blit(self.food.graphic,(25,25))
                         
             self.screen.fill((36,33,33))
-            # self.screen.fill((0,0,0))
-            self.screen.blit(self.food.graphic,self.food.pos)
-            # pygame.display.flip()
-            self.food.pos=(self.food.pos[0],self.food.pos[1]+1)
+           
+            # drawing food
+            # self.food.pos=(self.food.pos[0],self.food.pos[1]+1)
+            
+            # drawing snake
+            for pos in self.snake.pos:
+                self.screen.blit(self.snake.graphic,pos)
+            
+            self.screen.blit(self.food.graphic,(65,200)
+                            #  self.food.pos
+                             )
+            
             pygame.display.update()
         
 def main():
